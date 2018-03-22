d82497d127c46cdd65c1ffc498c8425a086115db
Fernanda Roxo <fernandaroxo@gmail.com>
2018-03-22 20:49:42 +0000

    Create open card list

--- js/app.js
+++ js/app.js
@@ -1,46 +1,46 @@
 /*
- * Create a list that holds all of your cards
- */
+* Create a list that holds all of your cards
+*/
 
- let cardArray = [
-     "fa-anchor",
-     "fa-anchor",
-     "fa-bicycle",
-     "fa-bicycle",
-     "fa-bolt",
-     "fa-bolt",
-     "fa-bomb",
-     "fa-bomb",
-     "fa-cube",
-     "fa-cube",
-     "fa-diamond",
-     "fa-diamond",
-     "fa-leaf",
-     "fa-leaf",
-     "fa-paper-plane-o",
-     "fa-paper-plane-o"
- ]
+let cardArray = [
+    "fa-anchor",
+    "fa-anchor",
+    "fa-bicycle",
+    "fa-bicycle",
+    "fa-bolt",
+    "fa-bolt",
+    "fa-bomb",
+    "fa-bomb",
+    "fa-cube",
+    "fa-cube",
+    "fa-diamond",
+    "fa-diamond",
+    "fa-leaf",
+    "fa-leaf",
+    "fa-paper-plane-o",
+    "fa-paper-plane-o"
+]
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+* Display the cards on the page
+*   - shuffle the list of cards using the provided "shuffle" method below
+*   - loop through each card and create its HTML
+*   - add each card's HTML to the page
+*/
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+   var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+   while (currentIndex !== 0) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex -= 1;
+       temporaryValue = array[currentIndex];
+       array[currentIndex] = array[randomIndex];
+       array[randomIndex] = temporaryValue;
+   }
 
-    return array;
+   return array;
 }
 
 const shuffledCards = shuffle(cardArray);
@@ -48,29 +48,37 @@ const shuffledCards = shuffle(cardArray);
 const deck = document.querySelector('.deck');
 
 for (let i = 0; i < shuffledCards.length; i++) {
-    deck.insertAdjacentHTML('beforeend', '<li class="card"><i class="fa ' + cardArray[i] + '"></i></li>');
+   deck.insertAdjacentHTML('beforeend', '<li class="card"><i class="fa ' + cardArray[i] + '"></i></li>');
 }
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+* set up the event listener for a card. If a card is clicked:
+*  - display the card's symbol (put this functionality in another function that you call from this one)
+*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*  - if the list already has another card, check to see if the two cards match
+*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*/
 
- deck.addEventListener('click', cardClick)
+let openCardList = [];
 
- function cardClick(event) {
-     if (event.target.nodeName === 'LI') {
-         const card = event.target.firstChild.classList[1];
-         turnCard(event.target);
-     }
- }
+deck.addEventListener('click', cardClick)
 
- function turnCard(element) {
-     element.classList.add('open', 'show');
- }
+function cardClick(event) {
+    if (event.target.nodeName === 'LI') {
+       const card = event.target.firstChild.classList[1];
+       turnCard(event.target);
+       listAsOpen(card);
+    }
+}
+
+function turnCard(element) {
+    element.classList.add('open', 'show');
+}
+
+function listAsOpen(card) {
+    openCardList.unshift(card);
+    console.log(openCardList);
+}
