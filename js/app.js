463c67456070e88e158ed7182d1e9624cdb398d9
Fernanda Roxo <fernandaroxo@gmail.com>
2018-03-26 20:41:17 +0100

    Add descriptive comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-/*
-* Create a list that holds all of your cards
-*/
-
+// Create an array to hold the 16 cards: 2 of each icon.
 let cardArray = [
     "fa-anchor",
     "fa-anchor",
@@ -21,13 +18,6 @@ let cardArray = [
     "fa-paper-plane-o"
 ]
 
-/*
-* Display the cards on the page
-*   - shuffle the list of cards using the provided "shuffle" method below
-*   - loop through each card and create its HTML
-*   - add each card's HTML to the page
-*/
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -45,23 +35,16 @@ return array;
 
 const shuffledCards = shuffle(cardArray);
 
+// After shuffling, add each card to the game by adding a li element
+// with the corresponding class to the deck ul.
+
 const deck = document.querySelector('.deck');
 
 for (let i = 0; i < shuffledCards.length; i++) {
     deck.insertAdjacentHTML('beforeend', '<li class="card"><i class="fa ' + cardArray[i] + '"></i></li>');
 }
 
-/*
-* set up the event listener for a card. If a card is clicked:
-*  - display the card's symbol (put this functionality in another function that you call from this one)
-*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-*  - if the list already has another card, check to see if the two cards match
-*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-*/
-
+// Create variables that will be needed in more than one function.
 let openCardList = [];
 let moveCount = 0;
 let matchedCards = 0;
@@ -77,17 +60,20 @@ const timerSpan = document.querySelector('.timer');
 deck.addEventListener('click', cardClick)
 
 function cardClick(event) {
-    // Only works if the target is a card (li element) and it's not open or matched.
+    // Only works if target is a card (li element) and it's not open or matched.
     // Doesn't work if 2 cards are already open (i.e. if player clicks on 3rd card too quickly).
     if (event.target.nodeName === 'LI' && !event.target.classList.contains('open') && !event.target.classList.contains('match') && openCardList.length < 2) {
+        // If it's the first move, start the counter
         if (moveCount === 0) {
             time = 0;
             timer = setInterval(changeTimer, 1000);
         }
+        // Turn card and add it to list of open cards
         const card = event.target.firstChild.classList[1];
         turnCard(event.target);
         listAsOpen(card);
-        if (openCardList.length >1) {
+        // If there's already an open card, see if the two match and run a function accordingly.
+        if (openCardList.length > 1) {
             if (openCardList.indexOf(card, 1) !== -1) {
                 lockCard();
             } else {
@@ -95,6 +81,7 @@ function cardClick(event) {
             }
         }
         incrementCounter();
+        // If all cards have been matched, stop timer and end game.
         if (matchedCards === 8) {
             clearInterval(timer);
             gameWon();
@@ -107,6 +94,7 @@ function changeTimer() {
     minutes = '0' + Math.floor(time/60).toString();
     seconds = '0' + (time % 60).toString();
     timerSpan.textContent = minutes + ':' + seconds.slice(-2);
+    // Expire game if it's not completed in 10 minutes
     if (time === 599) {
         clearInterval(timer);
         expire();
@@ -121,16 +109,18 @@ function listAsOpen(card) {
     openCardList.unshift(card);
 }
 
+// When cards match, change class to "match", increase count of matched cards and empty list.
 function lockCard() {
     const openCards = document.querySelectorAll('.open');
     for (let i = 0; i < 2; i++) {
         openCards[i].classList.add('match');
         openCards[i].classList.remove('open');
     }
-    openCardList = [];
     matchedCards += 1;
+    openCardList = [];
 }
 
+// When cards don't match, flip both over and empty list.
 function noMatch(card) {
     const openCards = document.querySelectorAll('.open');
     for (let i = 0; i < openCards.length; i++) {
@@ -139,6 +129,8 @@ function noMatch(card) {
     openCardList = [];
 }
 
+// Increment counter 0.5 per click, because a full move is comprised of
+// 2 clicks, that open 2 cards.
 function incrementCounter() {
     const moves = document.querySelector('.moves');
     moveCount += 0.5;
@@ -156,6 +148,8 @@ function incrementCounter() {
     }
 }
 
+// When game is own, remove the element that contains the game and replace it
+// with another that congratulates the player.
 function gameWon() {
     const finalScore = document.querySelector('.final-score');
     const containerWon = document.querySelector('.container-won');
@@ -165,6 +159,8 @@ function gameWon() {
     containerWon.children[2].append(stars);
 }
 
+// When game expires, remove the element that contains the game and replace it
+// with another that suggests the player starts again.
 function expire() {
     const containerExpire = document.querySelector('.container-expire');
     container.innerHTML = '';
